d314a243b8e77e637f3b9472e0dc23f3a4f01768
felipeciochetti@gmail.com <felipeciochetti@gmail.com>
2022-04-03 21:15:35 -0700

    add api

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const check = require('./check-case-status.js')
+const express = require('express')
+const cors = require('cors')
+ require('dotenv').config(); 
+const axios = require('express')
+
+
+const app = express();
+const PORT = 8000
+
+app.listen(PORT, () => console.log('Api is running on PORT ' + PORT))    
+
+
+app.get('/', (req,res) => {
+    res.json('USCIS TRACKER CASE CLEAN');
+})
+app.get('/consult-cases', (req,res) => {
+
+    console.log(req.query.number)
+    if(req.query.number === undefined){
+        res.send(500, 'Parameter Number is undefined ');
+        return;
+    }
+    if(req.query.field === undefined){
+        res.send(500, 'Parameter Field is undefined ');
+        return;
+    }
+    if(req.query.qtd === undefined){
+        res.send(500, 'Parameter qtd is undefined ');
+        return;
+    }
+
+    const field = req.query.field; // 'MSC'
+    const number = req.query.number; // number
+    const qtd = req.query.qtd;  // 10
+
+    check.checkCase(field,number,qtd).then(data => {
+
+        res.json(data);
+    
+      });
+
+    
+})
